e4ee629edb8e933a3311a9cb8b18dea12d941ce0
Geison <jjeisonalcan@gmail.com>
2024-06-16 23:04:36 -0500

    Actualización de archivos ci.yml, devops.ipynb y test_google.py

--- test_google.py
+++ test_google.py
@@ -1,17 +1,21 @@
 
 import pytest
+import time
 from selenium import webdriver
+from selenium.webdriver.common.by import By  # Importar By desde selenium.webdriver.common.by
 from selenium.webdriver.common.keys import Keys
 
 @pytest.fixture
 def browser():
-    driver = webdriver.Chrome()  # Asegrate de que ChromeDriver est en tu PATH
+    driver = webdriver.Chrome()
     yield driver
     driver.quit()
 
 def test_google_search(browser):
-    browser.get("https://www.google.com")
-    search_box = browser.find_element("name", "q")
-    search_box.send_keys("Selenium testing with Python")
+    browser.get('https://www.google.com/')
+    search_box = browser.find_element(By.NAME, 'q')  # Utilizar find_element con By.NAME
+    search_box.send_keys('pruebas automatizadas con DevOps')
     search_box.send_keys(Keys.RETURN)
-    assert "Selenium testing with Python" in browser.title
+    time.sleep(5)  # Esperar 5 segundos para ver los resultados
+    assert 'pruebas automatizadas con DevOps' in browser.title
+
