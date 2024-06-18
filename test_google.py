1dea1d701a55f1962bacc0e2e05538b7d80d2da2
Geison <jjeisonalcan@gmail.com>
2024-06-18 11:49:22 -0500

    Fix Selenium test configuration and update DevOps notebook

--- test_google.py
+++ test_google.py
@@ -1,8 +1,10 @@
 
+import pytest
+from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-from selenium import webdriver
 import logging
+import os
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -12,11 +14,16 @@ def test_google_search():
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--window-size=1920,1080")
-    chrome_options.add_argument("--headless")  # Ejecucin sin cabeza
+    chrome_options.add_argument("--headless")
     
-    # Ruta al chromedriver.exe
-    driver_path = 'D:/Disco Edinson/Downloads/chromedriver-win64/chromedriver.exe'
-    
+    # Obtener la ruta actual para asegurarse de la ubicacin
+    current_dir = os.getcwd()
+    logging.debug(f"Current directory: {current_dir}")
+
+    # Ruta al chromedriver.exe, usando ruta relativa desde el directorio principal del proyecto
+    driver_path = './drivers/chromedriver.exe'
+    logging.debug(f"Chromedriver path: {driver_path}")
+
     service = Service(driver_path)
     
     driver = webdriver.Chrome(service=service, options=chrome_options)
